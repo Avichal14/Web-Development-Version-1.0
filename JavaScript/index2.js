ec5b999faf5fb0593356c8f9f261980664069efb
Avichal14 <avichal.bharti14@gmail.com>
2023-06-08 00:09:37 +0530

    JS complete

---
+++ JavaScript/index2.js
@@ -0,0 +1,12 @@
+// console.log(Math.max(2, 3, 4, 5));
+
+// let lastName='Avichal';
+// let firstName= new String ('kumar');
+
+// let msg=`this is avichal
+// from btech
+// happy`;
+// console.log(msg);
+// console.log(Date);
+let date = new Date();
+console.log(date);
